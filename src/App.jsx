3646ceac83d2734c4258dfb0238a091607683a60
hase218 <chju23041@g.nihon-u.ac.jp>
2024-11-01 17:15:43 +0900

    画像をクリックしたときに関数が呼び出されるようにした

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,41 @@
 import { useState } from "react";
 
 export default function App() {
-    const [url, setUrl] = useState("images/pic1.jpg")
-    const imgs = [
-        {src: "images/pic1.jpg", alt: "Closeup of a human eye"},
-        {src: "images/pic2.jpg", alt: "Rock that looks like a wave"},
-        {src: "images/pic3.jpg", alt: "Purple and white pansies"},
-        {src: "images/pic4.jpg", alt: "Section of wall from a pharoah's tomb"},
-        {src: "images/pic5.jpg", alt: "Large moth on a leaf"}
-    ]
+  const [url, setUrl] = useState({src: "images/pic1.jpg", alt: "Closeup of a human eye"});
+  const imgs = [
+    {src: "images/pic1.jpg", alt: "Closeup of a human eye"},
+    {src: "images/pic2.jpg", alt: "Rock that looks like a wave"},
+    {src: "images/pic3.jpg", alt: "Purple and white pansies"},
+    {src: "images/pic4.jpg", alt: "Section of wall from a pharoah's tomb"},
+    {src: "images/pic5.jpg", alt: "Large moth on a leaf"}
+  ]
 
-    return (
-      <>
-
-        <h1>Image gallery example</h1>
-        <div className="full-img">
-          <img
-            className="displayed-img"
-            
-            alt="Closeup of a human eye"
-          />
-          
-          <div className="overlay"></div>
-          <button className="dark">Darken</button>
-        </div>
-        <div className="thumb-bar">
-            {imgs.map((img, index) => {
-            console.log(img,index)
-            return (
-                <img key={index} src={img.src} alt={img.alt} />
-            );
-          })}
-        </div>
-      </>
-    );
+  function handleClickImg() {
+    console.log("click");
   }
+
+  return (
+    <>
+      <h1>Image gallery example</h1>
+      <div className="full-img">
+      <img
+        className="displayed-img"
+        src="images/pic1.jpg"
+        alt="Purple and white pansies"
+      />
+          
+     <div className="overlay"></div>
+        <button className="dark">Darken</button>
+      </div>
+      <div className="thumb-bar">
+        {imgs.map((img, index) => {
+          console.log(img,index)
+          return (
+            <img onClick={handleClickImg} key={index} src={img.src} alt={img.alt} />
+         );
+        })}
+      </div>
+    </>
+  );
+}
 
