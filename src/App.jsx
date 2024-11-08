9743f15388eabd985b4e74351bbfcfdf5ccb9510
hase218 <chju23041@g.nihon-u.ac.jp>
2024-11-08 11:51:54 +0900

    darkenの動作加えた でも一回目だけエラー吐かれる

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 
 export default function App() {
   const [url, setUrl] = useState({src: "images/pic1.jpg", alt: "Closeup of a human eye"});
+  const [bgc, setBgc] = useState("darken");
   const imgs = [
     {src: "images/pic1.jpg", alt: "Closeup of a human eye"},
     {src: "images/pic2.jpg", alt: "Rock that looks like a wave"},
@@ -9,28 +10,37 @@ export default function App() {
     {src: "images/pic4.jpg", alt: "Section of wall from a pharoah's tomb"},
     {src: "images/pic5.jpg", alt: "Large moth on a leaf"}
   ]
+  const overlay=document.querySelector(".overlay");
 
   function handleClickDarkButton() {
     console.log("click");
-    
+    if(bgc==="darken"){
+      setBgc("lighten");
+      overlay.style.backgroundColor='rgba(0, 0, 0, 0.5)';
+    } else {
+      setBgc("darken");
+      overlay.style.backgroundColor='rgba(0, 0, 0, 0)';
+    }
   }
-
   
 
   return (
     <>
       <h1>Image gallery example</h1>
       <div className="full-img">
-      <img
-        className="displayed-img"
-        src={url.src}
-        alt={url.alt}
-      />
+        <img
+          className="displayed-img"
+          src={url.src}
+          alt={url.alt}
+        />
           
-     <div className="overlay">
+        <div className="overlay"></div>
 
-     </div>
-        <button onClick={handleClickDarkButton} className="dark">Darken</button>
+        <button onClick={handleClickDarkButton} 
+        // handleClickDarkButton呼び出すとき()つけなくていいのはなんで
+          className="dark">{bgc}
+        </button>
+        
       </div>
       <div className="thumb-bar">
         {imgs.map((img, index) => {
