7a15ca2b2c7f9a3907e032ba830c233ac47eb236
hase218 <chju23041@g.nihon-u.ac.jp>
2024-11-08 14:53:11 +0900

    いろいろ修正した。できた。

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,26 @@
 import { useState } from "react";
 
 export default function App() {
-  const [url, setUrl] = useState({src: "images/pic1.jpg", alt: "Closeup of a human eye"});
-  const [bgc, setBgc] = useState("darken");
+  const [url, setUrl] = useState({ src: "images/pic1.jpg", alt: "Closeup of a human eye" });
+  const [bright, setBright] = useState("darken");
   const imgs = [
-    {src: "images/pic1.jpg", alt: "Closeup of a human eye"},
-    {src: "images/pic2.jpg", alt: "Rock that looks like a wave"},
-    {src: "images/pic3.jpg", alt: "Purple and white pansies"},
-    {src: "images/pic4.jpg", alt: "Section of wall from a pharoah's tomb"},
-    {src: "images/pic5.jpg", alt: "Large moth on a leaf"}
+    { src: "images/pic1.jpg", alt: "Closeup of a human eye" },
+    { src: "images/pic2.jpg", alt: "Rock that looks like a wave" },
+    { src: "images/pic3.jpg", alt: "Purple and white pansies" },
+    { src: "images/pic4.jpg", alt: "Section of wall from a pharoah's tomb" },
+    { src: "images/pic5.jpg", alt: "Large moth on a leaf" }
   ]
-  const overlay=document.querySelector(".overlay");
 
   function handleClickDarkButton() {
     console.log("click");
-    if(bgc==="darken"){
-      setBgc("lighten");
-      overlay.style.backgroundColor='rgba(0, 0, 0, 0.5)';
+    if (bright === "darken") {
+      setBright("lighten");
     } else {
-      setBgc("darken");
-      overlay.style.backgroundColor='rgba(0, 0, 0, 0)';
+      setBright("darken");
     }
   }
-  
 
+  console.log(bright, bright==="darken" ? 0 : 0.5)
   return (
     <>
       <h1>Image gallery example</h1>
@@ -33,29 +30,27 @@ export default function App() {
           src={url.src}
           alt={url.alt}
         />
-          
-        <div className="overlay"></div>
 
-        <button onClick={handleClickDarkButton} 
-        // handleClickDarkButton呼び出すとき()つけなくていいのはなんで
-          className="dark">{bgc}
+        <div className="overlay" style={{backgroundColor:`rgba(0, 0, 0, ${bright==="darken" ? "0" : "0.5"})`}}></div>
+
+        <button onClick={handleClickDarkButton} className="dark">
+          {bright}
         </button>
-        
+
       </div>
       <div className="thumb-bar">
         {imgs.map((img, index) => {
           // console.log(img,index)
           return (
-            <img onClick={()=>{
+            <img onClick={() => {
               console.log("click", img);
               setUrl(img);
 
             }} key={index} src={img.src} alt={img.alt} />
-         );
-         //現在表示されている画像は選択できないように変更したいンゴねぇ
+          );
+          //現在表示されている画像は選択できないように変更したいンゴねぇ
         })}
       </div>
     </>
   );
 }
-
